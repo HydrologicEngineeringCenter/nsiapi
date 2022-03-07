aa7d7d9d34951e0265bf15a2bda784dbf3919551
TMNT <trietpersonal@gmail.com>
2022-03-07 15:46:37 -0500

    init + schema + devenv

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nsiapi
+
+This repo should be an extension from https://github.com/USACE/nsiv2-api with a new database schema allowing metadata and versioning
---
+++ cleandb.sql
@@ -0,0 +1,7 @@
+drop table dataset;
+drop table access;
+drop table quality;
+drop table domain;
+drop table schema_field;
+drop table nsi_schema;
+drop table nsi_field;
